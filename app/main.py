d3bfa92abd3594744dd7d70314307356fcebd9b3
Dominic Dawes <dominic.d.dawes@gmail.com>
2025-07-27 20:57:56 -0400

    ws imports

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ from fastapi import FastAPI, BackgroundTasks, HTTPException, WebSocket, WebSocke
 from fastapi.middleware.cors import CORSMiddleware
 
 # Websocket imports
-from ws_handlers import setup_websocket_routes, manager
+from ws_handlers.connection_managers import setup_websocket_routes, manager
 
 # Project modules
 from tasks.profile_tasks import upload_profile_picture_task
