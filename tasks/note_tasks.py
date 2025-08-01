6f83efda61e02ab7410f6ff21e7ee9070665f011
dawesdd1 <dominic.dawes@jhuapl.edu>
2025-08-01 13:11:42 -0400

    fixed note task supabase row name

--- tasks/note_tasks.py
+++ tasks/note_tasks.py
@@ -399,7 +399,7 @@ class AsyncNoteManager:
             await conn.execute(
                 """
                 INSERT INTO notes (
-                    id, user_id, project_id, title, content, 
+                    id, user_id, project_id, title, content_markdown, 
                     note_type, is_generated, is_shareable, created_at
                 ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
                 """,
